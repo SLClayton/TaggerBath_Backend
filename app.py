0d42a0e6818201ee8e07adf82a8a57afc66eca2f
Sam Clayton <samclaytonlewis@gmail.com>
2016-11-29 16:43:29 +0000

    Getting it to run

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import logging
+
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
@@ -14,4 +16,11 @@ def index():
 @app.errorhandler(404)
 def page_not_found(e):
     """Return a custom 404 error."""
+    logging.exception('An error occurred during a request.')
     return 'Sorry, nothing at this URL.', 404
+
+@app.errorhandler(500)
+def server_error(e):
+    # Log the error and stacktrace.
+    logging.exception('An error occurred during a request.')
+    return 'An internal error occurred.', 500
